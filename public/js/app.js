ff53a1cd94720ca8c049e7257a432463b6b10a7a
Alex Clarke <clarkexcore@gmail.com>
2019-10-17 18:30:45 -0400

    Changes before heroku push

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener("submit", (e) => {
     message_success.textContent = "";
     const location = search.value;
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+    fetch(`/weather?address=${location}`).then((res) => {
         res.json().then((data) => {
             if(data.error){
                 message_error.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define Paths for Express Config
 const public_directory = path.join(__dirname, '../public/');
@@ -101,6 +102,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
 });
